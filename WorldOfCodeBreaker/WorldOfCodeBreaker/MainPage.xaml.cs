bf8d2a927010d0867b2400b3e8efe63ebbd3fba4
AaronCronnelly <G00403528@gmit.ie>
2023-01-03 21:09:50 +0000

    figure out the out of bounds of array error, and got the grid working, now just need to make it so that the user can only access one row at a time

--- WorldOfCodeBreaker/WorldOfCodeBreaker/MainPage.xaml.cs
+++ WorldOfCodeBreaker/WorldOfCodeBreaker/MainPage.xaml.cs
@@ -15,11 +15,12 @@ namespace WorldOfCodeBreaker
 
 
         //CONSTATNS
-        const int ROWS = 2;
+        const int ROWS = 10;
         const int COLS = 4;
         const int PROWS = 2;
         const int PCOLS = 2;
 
+        //GLOBAL VARIABLE
         
 
         public MainPage()
@@ -30,7 +31,7 @@ namespace WorldOfCodeBreaker
 
         void BTNBegin_Clicked(System.Object sender, System.EventArgs e)
         {
-            makeGrid(0);
+            makeGrid();
             PegsGrid();//DISPLAYS USERS PEGS WHITE/BLACK
             BTNBegin.IsVisible = false;
             BTNCheck.IsVisible = true;
@@ -81,60 +82,74 @@ namespace WorldOfCodeBreaker
         }//END OF PEGSGRID
 
         private string[,] userAsnwer = new string[ROWS, COLS];
-        private void makeGrid(int row)
+        private void makeGrid()
         {
+           
             GameGrid.IsVisible = true;
-            for (int i = 0; i < COLS; i++)
+
+            for(int i=0; i<COLS; i++)
             {
                 GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
-
-                var userInput = new BoxView
-                {
-                    Margin = 5,
-                    HeightRequest = 40,
-                    WidthRequest = 40,
-                    HorizontalOptions = LayoutOptions.Start,
-                    VerticalOptions = LayoutOptions.Start,
-                    CornerRadius = 20,
-                    BackgroundColor = Color.Black,
-                    Opacity = 1,
-                };
-
-                var userChoice = new TapGestureRecognizer
-                {
-                    Command = new Command(() =>
-                    {
-                        if (userInput.BackgroundColor == Color.Black)
-                        {
-                            userInput.BackgroundColor = Color.Red;
-                            userAsnwer[row, i] = "red";//in theory this sould be userAnswer[0,0]="red";
-                        }
-                        else if (userInput.BackgroundColor == Color.Red)
-                        {
-                            userInput.BackgroundColor = Color.Blue;
-                           
-                        }
-                        else if (userInput.BackgroundColor == Color.Blue)
-                        {
-                            userInput.BackgroundColor = Color.Yellow;
-                            
-                        }
-                        else if (userInput.BackgroundColor == Color.Yellow)
-                        {
-                            userInput.BackgroundColor = Color.Green;
-                            
-                        }
-                        else
-                        {
-                            userInput.BackgroundColor = Color.Black;
-                        }
-
-                    })
-                };
-
-                userInput.GestureRecognizers.Add(userChoice);
-                GameGrid.Children.Add(userInput, i, 0);
             }
+
+            //GameGrid.RowDefinitions.Add(new RowDefinition());
+
+            for (int i = 0; i < ROWS; i++)
+            {
+                GameGrid.RowDefinitions.Add(new RowDefinition());
+                for (int j = 0; j < COLS; j++)
+                {
+                    //GameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                    var userInput = new BoxView
+                    {
+                        Margin = 5,
+                        HeightRequest = 30,
+                        WidthRequest = 30,
+                        HorizontalOptions = LayoutOptions.Start,
+                        VerticalOptions = LayoutOptions.Start,
+                        CornerRadius = 10,
+                        BackgroundColor = Color.Black,
+                        Opacity = 1,
+                    };
+
+                    var userChoice = new TapGestureRecognizer
+                    {
+                        Command = new Command(() =>
+                        {
+                            if (userInput.BackgroundColor == Color.Black)
+                            {
+                                userInput.BackgroundColor = Color.Red;
+                                userAsnwer[i%ROWS,j%COLS] = "red";//in theory this sould be userAnswer[0,0]="red";
+                            }
+                            else if (userInput.BackgroundColor == Color.Red)
+                            {
+                                userInput.BackgroundColor = Color.Blue;
+                                userAsnwer[i % ROWS, j % COLS] = "blue";
+                            }
+                            else if (userInput.BackgroundColor == Color.Blue)
+                            {
+                                userInput.BackgroundColor = Color.Yellow;
+                                userAsnwer[i % ROWS, j % COLS] = "yellow";
+                            }
+                            else if (userInput.BackgroundColor == Color.Yellow)
+                            {
+                                userInput.BackgroundColor = Color.Green;
+                                userAsnwer[i % ROWS, j % COLS] = "green";
+                            }
+                            else
+                            {
+                                userInput.BackgroundColor = Color.Black;
+                            }
+
+                        })
+                    };
+
+                    userInput.GestureRecognizers.Add(userChoice);
+                    GameGrid.Children.Add(userInput, j, i);
+                }
+            }
+            
         }//END OF MAKEGRID
 
         void BTNCheck_Clicked(System.Object sender, System.EventArgs e)
